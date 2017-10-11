89c3654f8e53243bf26d8f83af6b833e36e9dfca
Hack Reactor Student <student@hackreactor.com>
2017-10-11 12:21:24 -0700

    passing first few tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,29 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    
+    //index is the number of key-value pairs in the storage object, which is the index to push value parameter into
+    var index = Object.keys(storage).length;
+    // console.log('index ', index);
+    // console.log('storage ', storage);
+
+    storage[index] = value;
+
+    // console.log(storage);
+
+    //should return the new length?
+    // return storage;
   };
 
   someInstance.pop = function() {
+    var index = Object.keys(storage).length;
+    // console.log('pop ')
+    
+    delete storage[index];    
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
