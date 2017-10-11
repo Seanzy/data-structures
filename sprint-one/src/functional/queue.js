da0a8e6842d4245a7f971c93e1c5357e057c948c
Hack Reactor Student <student@hackreactor.com>
2017-10-11 13:25:25 -0700

    functional queue completed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,33 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // console.log('storage ', storage)
+
+    for (var key in storage) {
+      var newKey = Number(key) + 1;
+      storage[newKey] = storage[key];
+    }
+  
+    storage[0] = value;
+
+    // console.log('storage after ', storage);
+
+    // return storage;
+
   };
 
   someInstance.dequeue = function() {
+    var removed = storage[Object.keys(storage).length - 1];
+
+    delete storage[Object.keys(storage).length - 1];
+
+    return removed;
+  
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
+
   };
 
   return someInstance;
