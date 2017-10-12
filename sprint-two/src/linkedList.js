985772427bcc5346dcef2b6d83efc79c622aee3a
Hack Reactor Student <student@hackreactor.com>
2017-10-12 13:50:02 -0700

    LinkedList.js completed

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,70 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    console.log('value ' + value);
+    var newNode = new Node(value);
+    
+    if (list.head === null) {
+      list.head = newNode;
+    } 
+    
+    if (list.head !== null) {
+      list.head.next = newNode;
+      
+    }
+    
+    list.tail = newNode;
+  
   };
 
   list.removeHead = function() {
+    var removed = list.head;
+    
+    list.head = list.head.next;
+    
+    return removed.value;
+    
   };
 
   list.contains = function(target) {
+    
+  //   for (var key in list) {
+  //     var node = list[key];
+      
+  //     if (node.value === target) {
+  //       return true;
+  //     }
+  //   }
+    
+  //   return false;
+  // };
+  // var node = list.head
+  // var isItTarget = false
+  // while (node.next !== null) {
+  //   if(node.value === target) {
+  //     isItTarget = true;
+  //   }
+  // }
+  // return isItTarget
+  // return list;
+  
+  //recursive contains() function 
+  var checkTarget = function(LLobj, target) {
+    if (LLobj.value === target) {
+      return true;
+    } 
+    
+    if (LLobj.next === null) {
+      return false;
+    } else {
+      return checkTarget(LLobj.next, target);
+    }
+    
   };
 
+  return checkTarget(list.head, target);
+  };
+  
   return list;
 };
 
@@ -23,6 +79,7 @@ var Node = function(value) {
 
   return node;
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
