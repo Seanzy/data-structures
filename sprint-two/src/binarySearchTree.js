c74c4eabdaf5cb3776ace72d499d47554f5b6599
Hack Reactor Student <student@hackreactor.com>
2017-10-14 15:26:55 -0700

    binarySearchTree insert() completed

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,8 +1,56 @@
 var BinarySearchTree = function(value) {
+
+  var binaryTree = {};
+  
+  _.extend(binaryTree, binaryTreeMethods);
+  
+  binaryTree.value = value;
+  binaryTree.left = null;
+  binaryTree.right = null;
+  
+  return binaryTree;
   
 };
 
-//test
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(insertValue) {
+  var newNode = BinarySearchTree(insertValue);
+  
+  function searchBinaryTree(nodeObj) {
+    if (insertValue > nodeObj.value && nodeObj.right === null) {
+      nodeObj.right = newNode;
+    } else if (insertValue < nodeObj.value && nodeObj.left === null) {
+      nodeObj.left = newNode;
+    } else if (insertValue > nodeObj.value && nodeObj.right !== null) {
+      searchBinaryTree(nodeObj.right);
+    } else if (insertValue < nodeObj.value && nodeObj.left !== null) {
+      searchBinaryTree(nodeObj.left)
+    }
+  }
+  searchBinaryTree(this);
+
+};
+
+binaryTreeMethods.contains = function(value) {
+  var recursiveContains = function(node) {
+    if (value === node.value || value === node.left.value || value === node.right.value) {
+      return true;
+    } else if (node.left === null && node.right === null) {
+      return false;
+    } else if (node.left !== null && value < node.value) {
+      
+    }
+  }
+  
+};
+
+binaryTreeMethods.depthFirstLog = function(cb) {
+  
+};
+
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+//having trouble understanding where we instantiate a new node node using 'new', why, and how we refer to it
